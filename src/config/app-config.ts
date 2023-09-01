51636eb83e20c58511c972e3204db63f9f830e36
Kain <mtheusmss@gmail.com>
2023-09-01 01:56:02 -0300

    feature: make a config file

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,5 +1,6 @@
-export const API_URL = 'http://localhost:1337/';
-export const POSTS_URL = `${API_URL}/posts`;
+export const API_URL = 'http://localhost:1337/api';
+export const POSTS_URL = `${API_URL}/posts?populate=*`;
+export const CATEGORIES_URL = `${API_URL}/categories`;
 
 export const SITE_NAME = 'Pawllera';
-export const SITE_URL = '';
+export const SITE_URL = 'http://localhost:1337';
