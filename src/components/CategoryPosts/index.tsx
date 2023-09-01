cb8af9cd4147c0473785d01cfed3ef103d98ad94
Kain <mtheusmss@gmail.com>
2023-09-01 01:51:56 -0300

    feature: Component Category

---
+++ src/components/CategoryPosts/index.tsx
@@ -0,0 +1,35 @@
+import style from './CategoryPosts.module.scss';
+import { ICategoryInfo } from '@/interfaces/posts';
+import { useState } from 'react';
+
+export interface ICategoryProps {
+  categories: ICategoryInfo[];
+  handleFilter: (filter: string) => Promise<void>;
+}
+
+export const CategoryPosts = ({ categories, handleFilter }: ICategoryProps) => {
+  const [category, setCategory] = useState<string>('');
+
+  return (
+    <>
+      <ul className={style.list}>
+        {categories.map((category) => (
+          <li
+            className={style.filter}
+            key={category.id}
+            onClick={() => {
+              handleFilter(`${category.attributes.name}`);
+              setCategory(category.attributes.name);
+            }}
+          >
+            {category.attributes.name}
+          </li>
+        ))}
+      </ul>
+
+      <h1 className={`${style.title} ${category ? style.show : ''}`}>
+        {category}
+      </h1>
+    </>
+  );
+};
