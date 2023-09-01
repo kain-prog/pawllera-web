31d511e4b915687459e0dc4399c7dcfa9b99c044
Kain <mtheusmss@gmail.com>
2023-09-01 01:53:41 -0300

    feature: Component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,62 @@
+import Link from 'next/link';
+import logo from '../../../public/logo-pawllera.png';
+import Image from 'next/image';
+import style from './Header.module.scss';
+import { useRouter } from 'next/router';
+
 export const Header = () => {
+  const router = useRouter();
+
   return (
     <>
-      <div>teste</div>
+      <header className={style.header}>
+        <div className={style.container}>
+          <div className={style.logo}>
+            <Link href={'/'}>
+              <Image src={logo} alt="logo-pawllera" />
+            </Link>
+          </div>
+          <div className={style.nav}>
+            <nav>
+              <ul>
+                <li>
+                  <Link href="/about">
+                    <p
+                      className={
+                        router.pathname === '/about' ? `${style.active}` : ''
+                      }
+                    >
+                      Sobre
+                    </p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/projects">
+                    <p
+                      className={
+                        router.pathname === '/projects' ? `${style.active}` : ''
+                      }
+                    >
+                      Meus Projetos
+                    </p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/contact">
+                    <p
+                      className={
+                        router.pathname === '/contact' ? `${style.active}` : ''
+                      }
+                    >
+                      Contato
+                    </p>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
