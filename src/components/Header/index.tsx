44f8bfbeb64d33342cc8039b000306f0b2131b28
Kain <mtheusmss@gmail.com>
2023-09-04 03:52:41 -0300

    refactor: refactoring the component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,14 @@
-import Link from 'next/link';
-import logo from '../../../public/logo-pawllera.png';
-import Image from 'next/image';
 import style from './Header.module.scss';
 import { Nav } from '../Nav';
 import { Navbar } from '../ResponsiveNavbar';
+import { Logo } from '../Logo';
 
 export const Header = () => {
   return (
     <>
       <header className={style.header}>
         <div className={style.container}>
-          <div className={style.logo}>
-            <Link href={'/'}>
-              <Image src={logo} alt="logo-pawllera" />
-            </Link>
-          </div>
+          <Logo clProps="" />
           <Nav styleNav={`${style.navigation}`} />
           <Navbar />
         </div>
