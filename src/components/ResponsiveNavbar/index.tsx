1c134d25a14455b39f3eb41a0ec52e12e02997a5
Kain <mtheusmss@gmail.com>
2023-09-04 03:53:44 -0300

    feature: implements SocialMedia, Logo and Wpp comp

--- src/components/ResponsiveNavbar/index.tsx
+++ src/components/ResponsiveNavbar/index.tsx
@@ -1,19 +1,25 @@
 import { useState } from 'react';
 import style from './Navbar.module.scss';
 import { Nav } from '../Nav';
+import { SocialMedia } from '../SocialMedia';
+import { Logo } from '../Logo';
+import { Wpp } from '../Wpp';
 
 export const Navbar = () => {
   const [burgerClass, setBurgerClass] = useState<string>('unclicked');
   const [menuClass, setMenuClass] = useState<string>('hidden');
   const [isMenuClicked, setIsMenuClicked] = useState<boolean>(false);
+  const [fixedClass, setFixedClass] = useState<string>('fixed');
 
   const updateMenu = () => {
     if (!isMenuClicked) {
       setBurgerClass(style.clicked);
       setMenuClass(style.visible);
+      setFixedClass(style.fixed);
     } else {
       setBurgerClass(style.unclicked);
       setMenuClass(style.hidden);
+      setFixedClass('');
     }
     setIsMenuClicked(!isMenuClicked);
   };
@@ -21,14 +27,21 @@ export const Navbar = () => {
   return (
     <>
       <div className={style.containerSideBar}>
-        <div className={style.burgerMenu} onClick={updateMenu}>
+        <div
+          className={`${style.burgerMenu} ${fixedClass}`}
+          onClick={updateMenu}
+        >
           <div className={`${burgerClass} ${style.burgerBar}`}></div>
           <div className={`${burgerClass} ${style.burgerBar}`}></div>
           <div className={`${burgerClass} ${style.burgerBar}`}></div>
         </div>
 
         <div className={`${style.sideBar} ${menuClass}`}>
-          <Nav styleNav={style.column} />
+          <Logo clProps={style.logo} />
+          <Nav styleNav={style.menuNav} />
+          <SocialMedia clProps={style.social} />
+          <h3>Ou entre em contato pelo whatsapp</h3>
+          <Wpp clProps={style.wpp} />
         </div>
       </div>
     </>
