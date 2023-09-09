f71dae3a663333bf020d97125d9a2d567e468b58
Kain <mtheusmss@gmail.com>
2023-09-09 02:31:48 -0300

    feature: create a contactURL variable

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,6 +1,7 @@
 export const API_URL = 'http://localhost:1337/api';
 export const POSTS_URL = `${API_URL}/posts?populate=*`;
 export const CATEGORIES_URL = `${API_URL}/categories`;
+export const CONTACT_URL = `http://localhost:3000/api/contact`;
 
 export const SITE_NAME = 'Pawllera';
 export const SITE_URL = 'http://localhost:1337';
