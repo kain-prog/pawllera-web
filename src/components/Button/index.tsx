7512cea0846083936c9d8cff00d3f383ecf510c8
Kain <mtheusmss@gmail.com>
2023-09-11 03:24:59 -0300

    feature: Implements spinner

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import style from './Button.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FormEvent } from 'react';
 // import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 interface IButton {
@@ -9,13 +10,19 @@ interface IButton {
   redirect: string;
   content: string;
   iconFA: IconProp;
+  click: (e: FormEvent) => void;
 }
 
 export const Button = (props: IButton) => {
   return (
     <div className={`${style.container} ${props.clName}`}>
-      <Link href={props.redirect} className={style.btnPrimary}>
-        {props.content}
+      <Link
+        href={props.redirect}
+        className={style.btnPrimary}
+        onClick={props.click}
+      >
+        <span className={style.btnText}>{props.content}</span>
+        <span className={style.lSpinner}></span>
         <FontAwesomeIcon icon={props.iconFA} />
       </Link>
     </div>
