83b9c4858ce3bf619ea908bb819bf66f519a05ac
Kain <mtheusmss@gmail.com>
2023-10-02 03:12:37 -0300

    feaature: ending Loader screen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     setTimeout(() => {
-      setLoading(true);
+      setLoading(false);
     }, 2500);
   }, []);
 
